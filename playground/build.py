84e2e5010aca41b6b26565cd83990b4ba26af602
Daniel Odell <dmcodelljr@gmail.com>
2023-04-07 13:36:50 -0400

    Move links and add descriptions.

--- playground/build.py
+++ playground/build.py
@@ -6,6 +6,33 @@ page.add_header('Daniel Odell')
 
 page.add_text('Theoretical Nuclear Physicist')
 
+'''
+Links
+'''
+links = page.add_card()
+
+links.add_header('Links')
+
+links.add_link('GitHub', 'https://github.com/odell')
+links.add_text('I host most of the code for my projects on GitHub. mu2 and \
+rose are probably the most user-friendly if you\'re interested in \
+checking them out.')
+
+links.add_link('BAND Collaboration', 'https://bandframework.github.io')
+links.add_text('''
+Check out the exciting work we\'re doing in the Bayesian Analysis of Nuclear
+Dynamics collaboration (and the awesome people doing it.
+''')
+
+links.add_link('Mastodon', 'https://mastodon.social/@odell')
+links.add_text('''
+It's unlikely that I've posted anything here, but just in case...
+''')
+
+
+'''
+Resume
+'''
 resume = page.add_card()
 
 resume.add_header('Miniature Resumé')
@@ -27,13 +54,6 @@ resume.add_link('van der Waals Effective Field Theory | Phys. Rev. A (2021)',
                 'https://doi.org/10.1103/PhysRevA.104.023306')
 resume.add_link('Dissertation',
                 'https://inspirehep.net/files/415c336f4ff516359dd6bb2c70daa0cb')
-
-links = page.add_card()
-
-links.add_header('Links')
-links.add_link('GitHub', 'https://github.com/odell')
-links.add_link('BAND Collaboration', 'https://bandframework.github.io')
-links.add_link('Mastodon', 'https://mastodon.social/@odell')
 
 with open('index.html', 'w+') as f:
     f.write(page.to_html())
