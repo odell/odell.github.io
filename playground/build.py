7c4b54c45d9bd23f1d69c237c5b1a77cd5a88452
Daniel Odell <dmcodelljr@gmail.com>
2024-04-22 16:42:29 -0400

    Fix updated resume.

--- playground/build.py
+++ playground/build.py
@@ -36,16 +36,17 @@ Resume
 resume = page.add_card()
 
 resume.add_header('Miniature Resumé')
+resume.add_text('PhD Theoretical Physics, University of Tennessee (2019)')
+resume.add_text('BS Physics, Clemson University (2007)')
 
+resume.add_header('Education', size=3)
 resume.add_header('History', size=3)
 resume.add_text('Physicist, Savannah River National Laboratory (2023)')
-resume.add_text('Postdoctoral Researcher, Ohio University (Current)')
-resume.add_text('PhD Theoretical Physics, University of Tennessee (2019)')
+resume.add_text('Postdoctoral Researcher, Ohio University (2019)')
 resume.add_text('Research Assistant, University of Tennessee (2015)')
 resume.add_text('Teaching Assistant, University of Tennessee (2013)')
 resume.add_text('Teacher, South Aiken Christian School (2011)')
 resume.add_text('Scientist, Savannah River National Laboratory (2007)')
-resume.add_text('BS Physics, Clemson University (2007)')
 
 resume.add_header('Selected Publications', size=3)
 resume.add_link('19F(p, 𝛾)20Ne | Nature (2022)', 'https://doi.org/10.1038/s41586-022-05230-x')
